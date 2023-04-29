8ecf7c7d4daf5167e6a523071631b27bb5ad6a6f
Asterecho <2548299040@qq.com>
2023-04-29 12:30:54 +0800

    +

---
+++ Nixie/MainForm.Designer.cs
@@ -0,0 +1,237 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： ifwz
+ * 日期: 2023/4/28
+ * 时间: 20:23
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+namespace Nixie
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.PictureBox pictureBox4;
+		private System.Windows.Forms.PictureBox pictureBox5;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem 时钟模式ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+		private System.Windows.Forms.ToolStripMenuItem 随机排列5ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem 发电ToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem 退出ToolStripMenuItem;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.Timer timer2;
+		private System.Windows.Forms.PictureBox pictureBox6;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.时钟模式ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+			this.随机排列5ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.发电ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.退出ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.pictureBox4 = new System.Windows.Forms.PictureBox();
+			this.pictureBox5 = new System.Windows.Forms.PictureBox();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.timer2 = new System.Windows.Forms.Timer(this.components);
+			this.pictureBox6 = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.contextMenuStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.ContextMenuStrip = this.contextMenuStrip1;
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(6, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(46, 129);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox1MouseDown);
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.时钟模式ToolStripMenuItem,
+			this.toolStripMenuItem2,
+			this.随机排列5ToolStripMenuItem,
+			this.发电ToolStripMenuItem,
+			this.退出ToolStripMenuItem});
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(132, 114);
+			// 
+			// 时钟模式ToolStripMenuItem
+			// 
+			this.时钟模式ToolStripMenuItem.Name = "时钟模式ToolStripMenuItem";
+			this.时钟模式ToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+			this.时钟模式ToolStripMenuItem.Text = "时钟模式";
+			this.时钟模式ToolStripMenuItem.Click += new System.EventHandler(this.时钟模式ToolStripMenuItemClick);
+			// 
+			// toolStripMenuItem2
+			// 
+			this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+			this.toolStripMenuItem2.Size = new System.Drawing.Size(131, 22);
+			this.toolStripMenuItem2.Text = "20-20-20";
+			this.toolStripMenuItem2.Click += new System.EventHandler(this.ToolStripMenuItem2Click);
+			// 
+			// 随机排列5ToolStripMenuItem
+			// 
+			this.随机排列5ToolStripMenuItem.Name = "随机排列5ToolStripMenuItem";
+			this.随机排列5ToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+			this.随机排列5ToolStripMenuItem.Text = "随机排列5";
+			this.随机排列5ToolStripMenuItem.Click += new System.EventHandler(this.随机排列5ToolStripMenuItemClick);
+			// 
+			// 发电ToolStripMenuItem
+			// 
+			this.发电ToolStripMenuItem.Name = "发电ToolStripMenuItem";
+			this.发电ToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+			this.发电ToolStripMenuItem.Text = "发电";
+			this.发电ToolStripMenuItem.Click += new System.EventHandler(this.发电ToolStripMenuItemClick);
+			// 
+			// 退出ToolStripMenuItem
+			// 
+			this.退出ToolStripMenuItem.Name = "退出ToolStripMenuItem";
+			this.退出ToolStripMenuItem.Size = new System.Drawing.Size(131, 22);
+			this.退出ToolStripMenuItem.Text = "退出";
+			this.退出ToolStripMenuItem.Click += new System.EventHandler(this.退出ToolStripMenuItemClick);
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.ContextMenuStrip = this.contextMenuStrip1;
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(51, 0);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(46, 129);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox2.TabIndex = 1;
+			this.pictureBox2.TabStop = false;
+			this.pictureBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox2MouseDown);
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.ContextMenuStrip = this.contextMenuStrip1;
+			this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+			this.pictureBox3.Location = new System.Drawing.Point(93, 0);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(46, 129);
+			this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox3.TabIndex = 2;
+			this.pictureBox3.TabStop = false;
+			this.pictureBox3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox3MouseDown);
+			// 
+			// pictureBox4
+			// 
+			this.pictureBox4.ContextMenuStrip = this.contextMenuStrip1;
+			this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+			this.pictureBox4.Location = new System.Drawing.Point(134, 0);
+			this.pictureBox4.Name = "pictureBox4";
+			this.pictureBox4.Size = new System.Drawing.Size(46, 129);
+			this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox4.TabIndex = 3;
+			this.pictureBox4.TabStop = false;
+			this.pictureBox4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox4MouseDown);
+			// 
+			// pictureBox5
+			// 
+			this.pictureBox5.ContextMenuStrip = this.contextMenuStrip1;
+			this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+			this.pictureBox5.Location = new System.Drawing.Point(177, 0);
+			this.pictureBox5.Name = "pictureBox5";
+			this.pictureBox5.Size = new System.Drawing.Size(46, 129);
+			this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox5.TabIndex = 4;
+			this.pictureBox5.TabStop = false;
+			this.pictureBox5.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBox5MouseDown);
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 1000;
+			this.timer1.Tick += new System.EventHandler(this.Timer1Tick);
+			// 
+			// timer2
+			// 
+			this.timer2.Enabled = true;
+			this.timer2.Interval = 1000;
+			this.timer2.Tick += new System.EventHandler(this.Timer2Tick);
+			// 
+			// pictureBox6
+			// 
+			this.pictureBox6.Location = new System.Drawing.Point(6, 104);
+			this.pictureBox6.Name = "pictureBox6";
+			this.pictureBox6.Size = new System.Drawing.Size(217, 242);
+			this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.pictureBox6.TabIndex = 5;
+			this.pictureBox6.TabStop = false;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Black;
+			this.ClientSize = new System.Drawing.Size(229, 110);
+			this.ContextMenuStrip = this.contextMenuStrip1;
+			this.Controls.Add(this.pictureBox6);
+			this.Controls.Add(this.pictureBox5);
+			this.Controls.Add(this.pictureBox4);
+			this.Controls.Add(this.pictureBox3);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.pictureBox1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "MainForm";
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Nixie";
+			this.TopMost = true;
+			this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainFormMouseDown);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.contextMenuStrip1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+			this.ResumeLayout(false);
+
+		}
+	}
+}
---
+++ Nixie/MainForm.cs
@@ -0,0 +1,167 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： ifwz
+ * 日期: 2023/4/28
+ * 时间: 20:23
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Nixie
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		[DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        [DllImport("user32.dll")]
+        public static extern bool SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
+
+        private const int VM_NCLBUTTONDOWN = 0XA1;//定义鼠标左键按下
+        private const int HTCAPTION = 2;
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			pictureBox6.Image=Image.FromFile("image/relax.jpg");
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void 退出ToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+		void 发电ToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			System.Diagnostics.Process.Start("https://afdian.net/a/ifwz1729");
+		}
+		void 随机排列5ToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			Random ran = new Random();
+			int a = ran.Next(0, 10);
+			int b = ran.Next(0, 10);
+			int c = ran.Next(0, 10);
+			int d = ran.Next(0, 10);
+			int f = ran.Next(0, 10);
+			pictureBox1.Image=Image.FromFile("image/"+a+".png");
+			pictureBox2.Image=Image.FromFile("image/"+b+".png");
+			pictureBox3.Image=Image.FromFile("image/"+c+".png");
+			pictureBox4.Image=Image.FromFile("image/"+d+".png");
+			pictureBox5.Image=Image.FromFile("image/"+f+".png");
+			flag=false;
+			flag2=false;
+		}
+		bool flag=false;
+		void 时钟模式ToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			flag=true;
+			flag2=false;
+		}
+		void Timer1Tick(object sender, EventArgs e)
+		{
+			if (flag) {
+				string t=DateTime.Now.ToString("t");
+				pictureBox1.Image=Image.FromFile("image/"+t[0]+".png");
+				pictureBox2.Image=Image.FromFile("image/"+t[1]+".png");
+				pictureBox3.Image=Image.FromFile("image/"+"..png");
+				pictureBox4.Image=Image.FromFile("image/"+t[3]+".png");
+				pictureBox5.Image=Image.FromFile("image/"+t[4]+".png");
+			}
+		}
+		void ToolStripMenuItem2Click(object sender, EventArgs e)
+		{
+			flag=false;
+			flag2=true;
+			 
+		}
+		void MainFormMouseDown(object sender, MouseEventArgs e)
+		{
+			//为当前应用程序释放鼠标捕获
+            ReleaseCapture();
+            //发送消息 让系统误以为在标题栏上按下鼠标
+            SendMessage((IntPtr)this.Handle, VM_NCLBUTTONDOWN, HTCAPTION, 0);
+		}
+		void PictureBox1MouseDown(object sender, MouseEventArgs e)
+		{
+			//为当前应用程序释放鼠标捕获
+            ReleaseCapture();
+            //发送消息 让系统误以为在标题栏上按下鼠标
+            SendMessage((IntPtr)this.Handle, VM_NCLBUTTONDOWN, HTCAPTION, 0);
+		}
+		void PictureBox2MouseDown(object sender, MouseEventArgs e)
+		{
+			//为当前应用程序释放鼠标捕获
+            ReleaseCapture();
+            //发送消息 让系统误以为在标题栏上按下鼠标
+            SendMessage((IntPtr)this.Handle, VM_NCLBUTTONDOWN, HTCAPTION, 0);
+		}
+		void PictureBox3MouseDown(object sender, MouseEventArgs e)
+		{
+			//为当前应用程序释放鼠标捕获
+            ReleaseCapture();
+            //发送消息 让系统误以为在标题栏上按下鼠标
+            SendMessage((IntPtr)this.Handle, VM_NCLBUTTONDOWN, HTCAPTION, 0);
+		}
+		void PictureBox4MouseDown(object sender, MouseEventArgs e)
+		{
+			//为当前应用程序释放鼠标捕获
+            ReleaseCapture();
+            //发送消息 让系统误以为在标题栏上按下鼠标
+            SendMessage((IntPtr)this.Handle, VM_NCLBUTTONDOWN, HTCAPTION, 0);
+		}
+		void PictureBox5MouseDown(object sender, MouseEventArgs e)
+		{
+			//为当前应用程序释放鼠标捕获
+            ReleaseCapture();
+            //发送消息 让系统误以为在标题栏上按下鼠标
+            SendMessage((IntPtr)this.Handle, VM_NCLBUTTONDOWN, HTCAPTION, 0);
+		}
+		bool flag2=false;
+		TimeSpan ts = new TimeSpan(0, 20, 0);
+		void Timer2Tick(object sender, EventArgs e)
+		{
+			if (flag2) {
+				
+				ts = ts.Subtract(new TimeSpan(0, 0, 1));
+				string t=ts.ToString("T");
+				pictureBox1.Image=Image.FromFile("image/"+t[3]+".png");
+				pictureBox2.Image=Image.FromFile("image/"+t[4]+".png");
+				pictureBox3.Image=Image.FromFile("image/"+"..png");
+				pictureBox4.Image=Image.FromFile("image/"+t[6]+".png");
+				pictureBox5.Image=Image.FromFile("image/"+t[7]+".png");
+				
+				if (ts.TotalSeconds < 1.0) {
+					timer2.Enabled = false;
+					this.Height=240;
+					this.Location = new Point(this.Location.X,this.Location.Y-130);
+					pictureBox6.Location=new Point(6,0);
+					this.BackColor=Color.White;
+					this.Refresh();
+					Thread.Sleep(20*1000);
+					this.Location = new Point(this.Location.X,this.Location.Y+130);
+					pictureBox6.Location=new Point(6,104);
+					this.BackColor=Color.Black;
+					this.Height=110;
+					timer2.Enabled = true;
+					ts = new TimeSpan(0, 20, 0);
+				}
+			}
+		}
+		 
+		 
+		
+	}
+}
